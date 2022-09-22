2f7bb8569ca14f5fc8e2aab9dad9f64782191c13
muhammed samjidali <samjidali@gmail.com>
2022-09-22 17:16:30 +0530

    Second Commit

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,6 +40,14 @@ namespace API
 
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +65,7 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
